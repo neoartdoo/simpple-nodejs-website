2eb350e9e433240c13dae89a6353d8dc300c9569
Marjan Panić <panic.marjan@gmail.com>
2016-09-09 14:51:27 +0200

    Package.json

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var http = require('http'); //add the http module 
 var myServer = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type" :"text/plain"});
-    response.write("Hello");
+    response.writeHead(200, {"Content-Type" :"text/html"});
+    response.write("<h3>Hello and start your website</h3> ");
     response.end();
 }); // Create a server
 
 myServer.listen(2604);
+
+
+console.log("Open 127.0.0.1:2604 in your browser")
